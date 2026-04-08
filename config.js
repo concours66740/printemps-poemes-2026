0fa53863b13c99b43583dbb0999c8055b3db46db
Votre Nom <votre@email.com>
2026-04-08 12:28:00 +0200

    Description de ce que j'ai chang├®

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js - Fichier secret - NE PAS PARTAGER
+// À placer dans le même dossier que index.html
+
+const GITHUB_TOKEN = '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';
---
+++ inv.txt
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Description de ce que j'ai changé"
+git push
